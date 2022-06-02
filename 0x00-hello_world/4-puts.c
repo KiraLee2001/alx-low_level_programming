589273f6e33a1b9fe1715c7acb74f89d8f55ff02
KiraLee2001 <101580779+KiraLee2001@users.noreply.github.com>
2022-06-02 11:14:51 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - Function starts
  *
- * return - Function closes
+ * Return: Function closes
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}       
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+   
