861499c91c0c2ee541778171cc82874cf797f1bf
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 07:08:26 -0700

    letters except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,6 +2,7 @@
 
 /**
  * main - function starts
+ * Return: Always 0 (success/correct)
  */
 int main(void)
 {
@@ -9,7 +10,7 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if(c != 'e' && c != 'q')
+		if (c != 'e' && c != 'q')
 			putchar(c);
 	}
 	putchar('\n');
