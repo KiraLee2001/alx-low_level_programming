a0075fa71babe249e1b9d4f6b0bbebdf04595b2b
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 10:40:41 -0700

    spaces

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,23 +11,5 @@ int main(void)
 	int p;
 	int o;
 
-	for (p = 48; p <= 56; p++)
-	{
-		for (o = 49; 0 <= 57; o++)
-		{
-			if (o > p)
-				{
-					putchar(p);
-					putchar(o);
-					if (p != 56 || o != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+	for (p = 48
 
