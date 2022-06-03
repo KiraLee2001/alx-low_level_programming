0adbd220d5483ced1ea0b963cbb6c060a74520ce
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 07:19:00 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - point of entry
+ *
+ * Return: always 0  (success/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
