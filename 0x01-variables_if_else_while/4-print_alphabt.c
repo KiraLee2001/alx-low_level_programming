6ffdcd64e2b2e719974c1e1d526acfcf0b5e77c5
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 07:02:54 -0700

    letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function starts
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if(c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
