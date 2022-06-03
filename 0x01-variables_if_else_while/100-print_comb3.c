d3083585bb353e19ade32feb3e73f9cbaf159cd7
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 10:18:42 -0700

    numbers and commas

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+{
+	int p;
+	int o;
+
+	for (p = 48; p <= 56; p++)
+	{
+		for (o = 49; 0 <= 57; o++)
+		{
+			if (m > n)
+				{
+					putchar(p);
+					putchar(o);
+					if (p != 56 || m != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
