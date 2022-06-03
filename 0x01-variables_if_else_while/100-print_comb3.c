89a67540df7e663114b0b61e94674cd267496d2e
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 11:37:16 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,23 @@
 
 int main(void)
 {
-	int a;
-	int b;
+	int a, b;
 
 	for (a = 48; a < 58; a++)
 	{
-		for (b = 48; b < 58; b++)
+		for (b = 49; b < 58; b++)
 		{
 			if (a != b && a < b)
 			{
 				putchar(a);
 				putchar(b);
-				if (a == 57 || b == 56)
+				if (a == 56 && b == 57)
 				{
 					break
 						;
 				}
 				putchar(',');
-				putchar(' ');
+				putchar(',');
 			}
 		}
 	}
