f5eb4af87c1a134ece071d626b95a7646cb481d3
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-03 07:52:52 -0700

    printing in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - point of entry
+ *
+ * Return - Always 0 (success/correct)
+ */
+int main(void)
+{
+	char p;
+
+	for (p = 'z'; p >= 'a' p--)
+	{
+		putchar(p);
+	}
+	putchar('\n')
+	return (0);
+}
