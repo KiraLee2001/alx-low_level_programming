6d7615be9b0e9aecfe15293971b4221d6045ca1c
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-07 14:49:58 -0700

    negatives

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,24 @@
 #include "main.h"
 
 /**
- * _print_sign - return 0 letter not lowercase, 1 letter lowercase
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
  * @n : number to check
  * Return: 0 or 1
  */
-int _print_sign(int n)
+int print_sign(int n)
 {
-	int test;
-
 	if (n > 0)
-	{
-		test = 1;
+	
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		test = 0;
 		_putchar('0');
 	}
 	else
 	{
-		test = -1;
 		_putchar('-');
 	}
-	return (test);
+	return (-1);
 }
