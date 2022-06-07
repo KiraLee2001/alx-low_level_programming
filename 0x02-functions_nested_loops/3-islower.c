ed36a6fc08abb6ba53cdda07c3b77e1a9b97da02
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-07 07:37:27 -0700

    small letter checks

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/** 
+/**
  * _islower - checks for lowercase
  *
  * @c: parameter to check if characters are in lowercase
  *
- * Return: returns 1 if character is lower case and 0 if character is not lowercase'
+ * Return: returns 1 or 0 depending on checks'
  */
 int _islower(int k)
 
