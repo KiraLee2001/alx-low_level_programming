e11beb9ea410bc5989687534c6f95d3c103fbbae
KiraLee2001 <101580779+KiraLee2001@users.noreply.github.com>
2022-06-08 15:32:46 +0100

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,33 +1,47 @@
-
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
- * @n: print from this number
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-int a, b;
-if (n <= 98)
-{
-for (a = n; a <= 98; a++)
-{
-if (a != 98)
-printf("%d, ", a);
-else if (a == 98)
-printf("%d\n", a);
-}
-}
-else if (n >= 98)
-{
-for (b = n; b >= 98; b--)
-{
-if (b != 98)
-printf("%d, ", b);
-else if (b == 98)
-printf("%d\n", b);
-}
-}
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = b * a;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				} else if (c < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				} else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				} else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
