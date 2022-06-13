e44db24f991fcb1ad97735fb2d8054df7d7d28b1
KiraLee2001 <olonisayeprecious@gmail.com>
2022-06-13 07:55:10 -0700

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints stings in reverse
+ * @s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+	int b;
+
+	while (s[a] != '\0')
+	{
+		i++;
+	}
+	for (b = a - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
