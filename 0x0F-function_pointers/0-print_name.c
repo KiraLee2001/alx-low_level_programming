e142054db2f3c3f2cea614010b1203a6d5143b47
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-06 06:10:07 -0700

    printing name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - prints a name
  * @name: name to be printed
- * *f: pointer to printing function
+ * @f: pointer to printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
