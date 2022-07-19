6e54b849cc8197447887218f8afbb27ca60919c2
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-19 11:03:00 +0100

    changes

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 
 /**
- * add_nodeint - a function that adds a new node
- * at the beginning of a listint_t list
+ * add_nodeint - Adds a new node at the beginning
+ *               of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
  *
- * @head: head of the singly linked list
- *
- * @n: signly inked list elements
- *
- * Return: returns the address of the new element
- * and returns NULL on failure
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-struct listint_t *new_node;
-new_node = (listint_t *) malloc(sizeof(listint_t));
-if (new_node == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,34 +1,36 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - a function that adds a new node
- * at the end of a listint_t list
+ * add_nodeint_end - Adds a new node at the
+ *                   end of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
  *
- * @head: head of the singly linked list
- *
- * @n: singly linked list
- *
- * Return: returns the address of the new element
- * and on failure, reutrns NULL
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-struct listint_t *new_node;
-struct listint_t *current;
-current = *head;
-while (current && current->next != NULL)
-current = current->next;
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->n = n;
-new_node->next = NULL;
-if (current)
-current->next = new_node;
-else
-*head = new_node;
-return (new_node);
+	listint_t *new, *last;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+
+	return (*head);
 }
