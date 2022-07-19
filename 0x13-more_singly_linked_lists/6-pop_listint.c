6d74d39c898edf8878f9572858e222b9b7b3c5c3
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-19 11:12:33 +0100

    correction

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,26 @@
 #include "lists.h"
 
 /**
- * pop_listint - a function that deletes the head node of a listint_t
- * linked list, and returns the head node’s data (n)
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
  *
- * @head: the head of a singly linked list
- *
- * Return: returns the head node's data
- * and returns 0 if the linked list is empty
+ * Return: If the linked list is empty - 0.
+ *         Otherwise - The head node's data (n).
  */
 int pop_listint(listint_t **head)
 {
-struct listint_t *current;
-int n;
-if (*head != NULL)
-{
-current = *head;
-*head = (*(head))->next;
-n = current->n;
-free(current);
-return (n);
-}
-return (0);
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,30 +1,25 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - a function that returns the nth
- * node of a listint_t linked list
+ * get_nodeint_at_index - Locates a given node of
+ *                        a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ * @index: The index of the node to locate - indices start at 0.
  *
- * @head: the head f a singly linked list
- *
- * @index: the index of the node
- *
- * Return: the nth node of a linked list
- * and n failure, returns NULL
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the located node.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-struct listint_t *find_n;
-unsigned int next_node = 0;
-if (head != NULL)
-{
-find_n = head;
-while (find_n != NULL)
-{
-if (next_node == index)
-return (find_n);
-find_n = find_n->next;
-next_node++;
-}
-}
-return (NULL);
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
 }
