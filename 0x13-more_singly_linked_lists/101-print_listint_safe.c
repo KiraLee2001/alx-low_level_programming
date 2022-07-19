5589e869e85651060c82d4bc7dd625adf0dd5ae0
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-19 10:32:25 +0100

    correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,24 @@
 #include "lists.h"
 
 /**
- * main - check the code for ALX School students.
+ * print_listint_safe - Prints a linked list
+ * @h: The head of the linked list
  *
- * Return: Always 0.
+ * Return: The number of nodes in the list
  */
-int main(void)
+size_t print_listint_safe(const listint_t *head);
 {
-    listint_t *head;
-    listint_t *head2;
-    listint_t *node;
+	size_t numElem = 0;
+	const listint_t *headInit = NULL;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
-    head = NULL;
-    node = add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    node->next = add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint_safe(head);
-    return (0);
+	while (head != NULL && head != headInit)
+	{
+		if (headInit == NULL)
+			headInit = head;
+		printf("[%p] %d\n",(void *) head, head->n);
+		head = head->next;
+		numElem++;
+	}
+
+	return (numElem);
 }
